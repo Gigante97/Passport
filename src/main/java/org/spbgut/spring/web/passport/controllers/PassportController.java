a1a519a3f1701372e7bee7335d55022d44c75452
kirill <kdgigante@gmail.com>
2023-05-20 16:00:54 +0300

    final version 4.0 with postgres add docker-compose.yml

--- src/main/java/org/spbgut/spring/web/passport/controllers/PassportController.java
+++ src/main/java/org/spbgut/spring/web/passport/controllers/PassportController.java
@@ -23,9 +23,6 @@ public class PassportController {
 
     @GetMapping("/{number}")
     public Optional<PassportDto> getProductByNumber(@PathVariable Integer number) {
-       // Passport passport = passportService.findByNumber(number).orElseThrow(() -> new ResourceNotFoundException("Product not find" + number));
-        //return passport;
-        //PassportDto passportDto = passportService.findByNumber(number).map(p->passportConverter.entityToDto(p)).orElseThrow(()->new ResourceNotFoundException("Product not found with id: " + number));
         passportService.findByNumber(number).orElseThrow(()->new ResourceNotFoundException("Product not found with id: " + number));
         return passportService.findByNumber(number).map(p->passportConverter.entityToDto(p));
     }
